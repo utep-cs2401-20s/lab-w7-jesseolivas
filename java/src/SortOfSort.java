d256c307114951fde3885f1428ee38fc1f07d441
jesseolivas <jesseolivas@live.com>
2020-03-04 22:59:43 -0700

    Forgot to remove main method created for testing while working on method, now all is finished except time complexity documentation.

--- java/src/SortOfSort.java
+++ java/src/SortOfSort.java
@@ -47,13 +47,4 @@ public class SortOfSort {
         }
         return a;
     }
-
-    public static void main(String[] args) {
-        int[] a = {4,3,2,1,6,5};
-        sortOfSort(a);
-        for(int i= 0; i < a.length;i++){
-            System.out.print(a[i]);
-        }
-
-    }
 }
